b1ec6bdfc3ca008ca29e2f5d890cddd0ed6fade9
rafaelgarcezdev <rafaelgarcez02@gmail.com>
2025-01-04 23:08:50 -0300

    atualizando

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 AOS.init();
+
 $(".owl-carousel").owlCarousel({
   loop: false,
   margin: 15,
   responsiveClass: true,
-
   dots: true,
   nav: true,
   navText: [
-    "<div class='custom-prev'>‹</div>",
-    "<div class='custom-next'>›</div>",
+    "<button class='custom-prev' aria-label='Anterior'>‹</button>",
+    "<button class='custom-next' aria-label='Próximo'>›</button>",
   ],
   responsive: {
     0: {
@@ -21,4 +21,8 @@ $(".owl-carousel").owlCarousel({
       items: 3,
     },
   },
+});
+
+document.querySelectorAll(".custom-prev, .custom-next").forEach((button) => {
+  button.removeAttribute("role");
 });
